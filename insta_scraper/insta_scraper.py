074ee7312a036751217c076c5ae9931d1ff55cfc
Shakeel95 <41747714+Shakeel95@users.noreply.github.com>
2020-06-05 20:58:55 +0100

    BLACK!

--- insta_scraper/insta_scraper.py
+++ insta_scraper/insta_scraper.py
@@ -12,6 +12,7 @@ import getpass
 import time
 import re
 
+
 class insta_scraper:
     """
     Data collection class for Lorenzo's thesis...
@@ -27,13 +28,12 @@ class insta_scraper:
         - scrape_post_data: scrapes posts for target account
     """
 
-
     def __init__(self,):
 
         self.driver = None
         self.insta_url = "https://www.instagram.com"
 
-        self.target_metadata = dict(post = None, followers = None, following = None)
+        self.target_metadata = dict(post=None, followers=None, following=None)
         self.target_handle = None
         self.target_url = None
         self.target_follower_list = None
@@ -41,18 +41,19 @@ class insta_scraper:
 
         self.df = None
         self.dff = None
-        self.df_colnames = ["follows_target",
-                            "n_followers",
-                            "n_following",
-                            "n_posts",
-                            "posts_over_time",
-                            "bio",
-                            "post_1_content",
-                            "post_2_content",
-                            "post_3_content"]
+        self.df_colnames = [
+            "follows_target",
+            "n_followers",
+            "n_following",
+            "n_posts",
+            "posts_over_time",
+            "bio",
+            "post_1_content",
+            "post_2_content",
+            "post_3_content",
+        ]
 
-
-    def start_driver(self,path_to_driver, headless = False):
+    def start_driver(self, path_to_driver, headless=False):
         """
         Starts webdriver and performs login.
         -----
@@ -67,16 +68,20 @@ class insta_scraper:
         options = webdriver.ChromeOptions()
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--incognito")
-        if headless: options.add_argument("--headless")
+        if headless:
+            options.add_argument("--headless")
 
-        self.driver = webdriver.Chrome(path_to_driver, options = options)
+        self.driver = webdriver.Chrome(path_to_driver, options=options)
 
         self.driver.get(self.insta_ulr)
 
-        WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.NAME,"username"))).send_keys(uname)
-        self.driver.find_element_by_name("password").send_keys(getpass.getpass("Password: "))
+        WebDriverWait(self.driver, 5).until(
+            ec.presence_of_element_located((By.NAME, "username"))
+        ).send_keys(uname)
+        self.driver.find_element_by_name("password").send_keys(
+            getpass.getpass("Password: ")
+        )
         self.driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
-
 
     def quit_driver(self,):
         """
@@ -85,8 +90,7 @@ class insta_scraper:
 
         self.driver.quit()
 
-
-    def set_follower_list(self,path):
+    def set_follower_list(self, path):
         """
         Sets follower list for target account; must be collected externally.
         -----
@@ -96,10 +100,11 @@ class insta_scraper:
             - header:
         """
 
-        self.target_follower_list = pd.read_csv(path, header = header)[col_index].to_list()
+        self.target_follower_list = pd.read_csv(path, header=header)[
+            col_index
+        ].to_list()
 
-
-    def target_accout(self,account):
+    def target_accout(self, account):
         """
         Navigates to account and collects accoutn metadata
         -----
@@ -107,7 +112,7 @@ class insta_scraper:
             - account: string, inta handle
         """
 
-        self.driver.get(os.path.join(self.insta_ulr,account))
+        self.driver.get(os.path.join(self.insta_ulr, account))
 
         self.target_handle = account
         self.target_url = self.driver.current_url
@@ -119,8 +124,7 @@ class insta_scraper:
         self.target_metadatap["followers"] = banner[1].text.replace(",", "")
         self.target_metadatap["following"] = banner[2].text.replace(",", "")
 
-
-    def scrape_post_data(self, post_index, max_likes = 300,scroll_pause = 0.5):
+    def scrape_post_data(self, post_index, max_likes=300, scroll_pause=0.5):
         """
         What it does ....
         -----
@@ -145,39 +149,56 @@ class insta_scraper:
 
         # navigate to indexed post
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
-        post = soup.find_all("div", class_ = "v1Nh3 kIKUG _bz0w")[post_index-1]
-        self.driver.get(os.path.join(self.insta_ulr, post.find(href = True)["href"][1:]))
+        post = soup.find_all("div", class_="v1Nh3 kIKUG _bz0w")[post_index - 1]
+        self.driver.get(os.path.join(self.insta_ulr, post.find(href=True)["href"][1:]))
 
         # set up data collection
-        self.dff = pd.DataFrame(columns = df_colnames + ["likes_post_{}".format(post_index), "comments_post_{}".format(post_index)])
+        self.dff = pd.DataFrame(
+            columns=df_colnames
+            + [
+                "likes_post_{}".format(post_index),
+                "comments_post_{}".format(post_index),
+            ]
+        )
 
         # find users commenting
-        soup = BeautifulSoup(self.driver.page_source,"html.parser")
-        users = soup.find("ul",{"class":"Mr508"})
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        users = soup.find("ul", {"class": "Mr508"})
 
         for user in users:
 
-            handle = user.find(href = True)["href"].strip("/")
+            handle = user.find(href=True)["href"].strip("/")
             self.dff.loc[handle] = [0 for i in range(len(self.dff.columns))]
-            self.dff["comments_post_{}".format(post_index)][handle] = user.find("div",{"class":"C4VMK"}).text.strip(handle)
+            self.dff["comments_post_{}".format(post_index)][handle] = user.find(
+                "div", {"class": "C4VMK"}
+            ).text.strip(handle)
 
             if handle in self.target_follower_list:
                 self_df["follows_target"][handle] = 1
 
         # find users liking
         likes = self.driver.find_element_by_class_name("Nm9Fw").text.strip(" likes")
-        WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.XPATH,"//button[@class='sqdOP yWX7d     _8A5w5    ']"))).click()
+        WebDriverWait(self.driver, 5).until(
+            ec.presence_of_element_located(
+                (By.XPATH, "//button[@class='sqdOP yWX7d     _8A5w5    ']")
+            )
+        ).click()
 
         runs = []
-        old_len = 0; new_len = 0;
+        old_len = 0
+        new_len = 0
         i = 0
 
-        while (len(self.dff) < min(max_likes,int(likes))):
+        while len(self.dff) < min(max_likes, int(likes)):
 
-            scroll_bar = WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.XPATH,"//div[@style='height: 356px; overflow: hidden auto;']")))
+            scroll_bar = WebDriverWait(self.driver, 5).until(
+                ec.presence_of_element_located(
+                    (By.XPATH, "//div[@style='height: 356px; overflow: hidden auto;']")
+                )
+            )
 
-            soup = BeautifulSoup(self.driver.page_source,"html.parser")
-            users = soup.find_all("div", {"aria-labelledby" : re.compile(r".*")})
+            soup = BeautifulSoup(self.driver.page_source, "html.parser")
+            users = soup.find_all("div", {"aria-labelledby": re.compile(r".*")})
 
             for user in users:
                 handle = user.find("a")["href"].strip("/")
@@ -191,20 +212,21 @@ class insta_scraper:
                     if handle in self.target_follower_list:
                         self.dff["follows_target"][handle] = 1
 
-            old_len = new_len; new_len = len(self_df)
-            if (old_len == new_len):
+            old_len = new_len
+            new_len = len(self_df)
+            if old_len == new_len:
                 runs += [1]
-            else :
+            else:
                 runs += [0]
-            if (sum(runs[-4:]) > 3):
+            if sum(runs[-4:]) > 3:
                 break
 
             self.driver.execute_script(
-            "arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
-            scroll_bar,
+                "arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
+                scroll_bar,
             )
 
             if (i % 7) == 0:
-                self.driver.execute_script("arguments[0].scrollTop -= 400;",scroll_bar)
+                self.driver.execute_script("arguments[0].scrollTop -= 400;", scroll_bar)
             i += 1
             time.sleep(scroll_pause)
--- insta_scraper/scraper.py
+++ insta_scraper/scraper.py
@@ -16,6 +16,7 @@ import time
 import re
 import datetime
 
+
 class insta_scraper:
     """
     Data collection class for Lorenzo's thesis...
@@ -33,7 +34,6 @@ class insta_scraper:
         - scrape_post_data: scrapes posts for target account
     """
 
-
     def __init__(self,):
 
         self.driver = None
@@ -41,25 +41,26 @@ class insta_scraper:
         self.uname = None
         self.pwd = None
 
-        self.target_metadata = dict(post = None, followers = None, following = None)
+        self.target_metadata = dict(post=None, followers=None, following=None)
         self.target_handle = None
         self.target_url = None
         self.target_follower_list = None
         self.posts_scraped = []
 
         self.df = None
-        self.df_colnames = ["follows_target",
-                            "n_followers",
-                            "n_following",
-                            "n_posts",
-                            "posts_over_time",
-                            "bio",
-                            "post_1_content",
-                            "post_2_content",
-                            "post_3_content"]
+        self.df_colnames = [
+            "follows_target",
+            "n_followers",
+            "n_following",
+            "n_posts",
+            "posts_over_time",
+            "bio",
+            "post_1_content",
+            "post_2_content",
+            "post_3_content",
+        ]
 
-
-    def start_driver(self,path_to_driver, headless = False):
+    def start_driver(self, path_to_driver, headless=False):
         """
         Starts webdriver and performs login.
         -----
@@ -74,18 +75,20 @@ class insta_scraper:
         options = webdriver.ChromeOptions()
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--incognito")
-        if headless: options.add_argument("--headless")
+        if headless:
+            options.add_argument("--headless")
 
-        self.driver = webdriver.Chrome(path_to_driver, options = options)
+        self.driver = webdriver.Chrome(path_to_driver, options=options)
 
         self.driver.get(self.insta_url)
 
         self.uname = input("insta username: ")
-        WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.NAME,"username"))).send_keys(self.uname)
+        WebDriverWait(self.driver, 5).until(
+            ec.presence_of_element_located((By.NAME, "username"))
+        ).send_keys(self.uname)
         self.pwd = getpass.getpass("insta password: ")
         self.driver.find_element_by_name("password").send_keys(self.pwd)
         self.driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
-
 
     def quit_driver(self,):
         """
@@ -94,8 +97,7 @@ class insta_scraper:
 
         self.driver.quit()
 
-
-    def string2float(self,string):
+    def string2float(self, string):
         """
         Jesus turned water into wine. This function turns trings into floats.
         Your move Jesus.
@@ -105,9 +107,9 @@ class insta_scraper:
         """
 
         if "m" in string:
-            return 1000000*float(string.strip("m"))
+            return 1000000 * float(string.strip("m"))
         elif "k" in string:
-            return 1000*float(string.strip("k"))
+            return 1000 * float(string.strip("k"))
         else:
             try:
                 f = float(string)
@@ -115,8 +117,7 @@ class insta_scraper:
             except:
                 return np.Inf
 
-
-    def TagDateRatio(self,tags,epsilon=0.1):
+    def TagDateRatio(self, tags, epsilon=0.1):
         """
         Calculates posts per day.
         -----
@@ -133,17 +134,20 @@ class insta_scraper:
         dates = []
         for tag in tags:
             try:
-                dates += [parser.parse(re.search("on (.*)\. ", tag.find("img")["alt"]).group(1))]
+                dates += [
+                    parser.parse(
+                        re.search("on (.*)\. ", tag.find("img")["alt"]).group(1)
+                    )
+                ]
             except:
                 pass
 
         if len(dates) == 0:
             return 1
         else:
-            return len(dates)/((dates[0]-dates[-1]).days+epsilon)
+            return len(dates) / ((dates[0] - dates[-1]).days + epsilon)
 
-
-    def set_follower_list(self,path, header, col_index):
+    def set_follower_list(self, path, header, col_index):
         """
         Sets follower list for target account; must be collected externally.
         -----
@@ -153,10 +157,11 @@ class insta_scraper:
             - header: whether the csv contains a header
         """
 
-        self.target_follower_list = pd.read_csv(path, header = header)[col_index].to_list()
+        self.target_follower_list = pd.read_csv(path, header=header)[
+            col_index
+        ].to_list()
 
-
-    def target_accout(self,account):
+    def target_accout(self, account):
         """
         Navigates to account and collects accoutn metadata
         -----
@@ -164,7 +169,7 @@ class insta_scraper:
             - account: string, inta handle
         """
 
-        self.driver.get(os.path.join(self.insta_url,account))
+        self.driver.get(os.path.join(self.insta_url, account))
 
         self.target_handle = account
         self.target_url = self.driver.current_url
@@ -176,10 +181,9 @@ class insta_scraper:
         self.target_metadata["followers"] = banner[1].text.replace(",", "")
         self.target_metadata["following"] = banner[2].text.replace(",", "")
 
-        self.df = self.df = pd.DataFrame(columns = self.df_colnames)
+        self.df = self.df = pd.DataFrame(columns=self.df_colnames)
 
-
-    def scrape_post_data(self, post_index, max_likes = 300,scroll_pause = 0.5):
+    def scrape_post_data(self, post_index, max_likes=300, scroll_pause=0.5):
         """
         What it does ....
         -----
@@ -202,11 +206,10 @@ class insta_scraper:
         if post_index in self.posts_scraped:
             return "Data for post {} already collected".format(post_index)
 
-
         # navigate to indexed post
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
-        post = soup.find_all("div", class_ = "v1Nh3 kIKUG _bz0w")[post_index-1]
-        self.driver.get(os.path.join(self.insta_url, post.find(href = True)["href"][1:]))
+        post = soup.find_all("div", class_="v1Nh3 kIKUG _bz0w")[post_index - 1]
+        self.driver.get(os.path.join(self.insta_url, post.find(href=True)["href"][1:]))
         time.sleep(1)
 
         # SET VIDEO CONDITION!
@@ -218,66 +221,79 @@ class insta_scraper:
         self.df["comments_post_{}".format(post_index)] = 0
 
         # find users commenting
-        soup = BeautifulSoup(self.driver.page_source,"html.parser")
-        users = soup.find_all("ul",{"class":"Mr508"})
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        users = soup.find_all("ul", {"class": "Mr508"})
 
         for user in users:
-            handle = user.find(href = True)["href"].strip("/")
+            handle = user.find(href=True)["href"].strip("/")
 
             if (self.df.index == handle).any():
-                self.df.loc[handle,"comments_post_{}".format(post_index)] = user.find("div",{"class":"C4VMK"}).text.strip(handle)
+                self.df.loc[handle, "comments_post_{}".format(post_index)] = user.find(
+                    "div", {"class": "C4VMK"}
+                ).text.strip(handle)
             else:
                 self.df.loc[handle] = 0
-                self.df.loc[handle, "comments_post_{}".format(post_index)] = user.find("div",{"class":"C4VMK"}).text.strip(handle)
+                self.df.loc[handle, "comments_post_{}".format(post_index)] = user.find(
+                    "div", {"class": "C4VMK"}
+                ).text.strip(handle)
 
             if handle in self.target_follower_list:
-                self.df.loc[handle,"follows_target"] = 1
+                self.df.loc[handle, "follows_target"] = 1
 
         # find users liking
         likes = self.driver.find_element_by_class_name("Nm9Fw").text.strip(" likes")
-        WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.XPATH,"//button[@class='sqdOP yWX7d     _8A5w5    ']"))).click()
+        WebDriverWait(self.driver, 5).until(
+            ec.presence_of_element_located(
+                (By.XPATH, "//button[@class='sqdOP yWX7d     _8A5w5    ']")
+            )
+        ).click()
 
         runs = []
-        old_len = 0; new_len = 0;
+        old_len = 0
+        new_len = 0
         i = 0
 
-        while (len(self.df) < min(max_likes,int(likes))):
+        while len(self.df) < min(max_likes, int(likes)):
 
-            scroll_bar = WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.XPATH,"//div[@style='height: 356px; overflow: hidden auto;']")))
+            scroll_bar = WebDriverWait(self.driver, 5).until(
+                ec.presence_of_element_located(
+                    (By.XPATH, "//div[@style='height: 356px; overflow: hidden auto;']")
+                )
+            )
 
-            soup = BeautifulSoup(self.driver.page_source,"html.parser")
-            users = soup.find_all("div", {"aria-labelledby" : re.compile(r".*")})
+            soup = BeautifulSoup(self.driver.page_source, "html.parser")
+            users = soup.find_all("div", {"aria-labelledby": re.compile(r".*")})
 
             for user in users:
                 handle = user.find("a")["href"].strip("/")
 
                 if (self.df.index == handle).any():
-                    self.df.loc[handle,"likes_post_{}".format(post_index)] = 1
+                    self.df.loc[handle, "likes_post_{}".format(post_index)] = 1
 
                 else:
                     self.df.loc[handle] = 0
-                    self.df.loc[handle,"likes_post_{}".format(post_index)] = 1
+                    self.df.loc[handle, "likes_post_{}".format(post_index)] = 1
                     if handle in self.target_follower_list:
-                        self.df.loc[handle,"follows_target"] = 1
+                        self.df.loc[handle, "follows_target"] = 1
 
-            old_len = new_len; new_len = len(self.df)
-            if (old_len == new_len):
+            old_len = new_len
+            new_len = len(self.df)
+            if old_len == new_len:
                 runs += [1]
-            else :
+            else:
                 runs += [0]
-            if (sum(runs[-4:]) > 3):
+            if sum(runs[-4:]) > 3:
                 break
 
             self.driver.execute_script(
-            "arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
-            scroll_bar,
+                "arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;",
+                scroll_bar,
             )
 
             if (i % 7) == 0:
-                self.driver.execute_script("arguments[0].scrollTop -= 400;",scroll_bar)
+                self.driver.execute_script("arguments[0].scrollTop -= 400;", scroll_bar)
             i += 1
             time.sleep(scroll_pause)
-
 
     def scrape_user_data(self,):
         """
@@ -288,43 +304,61 @@ class insta_scraper:
 
         for handle in tqdm(self.df.index.to_list()):
 
-            self.driver.get(os.path.join(self.insta_url,handle))
-            time.sleep(np.random.exponential(1,1))
+            self.driver.get(os.path.join(self.insta_url, handle))
+            time.sleep(np.random.exponential(1, 1))
 
             # followers, following, posts
             soup = BeautifulSoup(self.driver.page_source, "html.parser")
             banner = soup.find_all(class_="g47SY")
-            self.df.loc[handle,"n_followers"] = self.string2float(banner[1].text.replace(",", ""))
-            self.df.loc[handle,"n_following"] = self.string2float(banner[2].text.replace(",", ""))
-            self.df.loc[handle,"n_posts"] = self.string2float(banner[0].text.replace(",", ""))
+            self.df.loc[handle, "n_followers"] = self.string2float(
+                banner[1].text.replace(",", "")
+            )
+            self.df.loc[handle, "n_following"] = self.string2float(
+                banner[2].text.replace(",", "")
+            )
+            self.df.loc[handle, "n_posts"] = self.string2float(
+                banner[0].text.replace(",", "")
+            )
 
             # bio
             try:
-                self.df.loc[handle,"bio"] = soup.find("div",{"class":"-vDIg"}).find("span").text
+                self.df.loc[handle, "bio"] = (
+                    soup.find("div", {"class": "-vDIg"}).find("span").text
+                )
             except:
-                self.df.loc[handle,"bio"] = np.NaN
+                self.df.loc[handle, "bio"] = np.NaN
 
             # posts over time
-            posts = soup.find_all("div",{"class":"KL4Bh"})
-            self.df.loc[handle,"posts_over_time"] = self.TagDateRatio(posts)
+            posts = soup.find_all("div", {"class": "KL4Bh"})
+            self.df.loc[handle, "posts_over_time"] = self.TagDateRatio(posts)
 
             # post content
-            if soup.find("div",{"class":"Nnq7C weEfm"}) == None :
+            if soup.find("div", {"class": "Nnq7C weEfm"}) == None:
                 continue
             else:
-                links = soup.find("div",{"class":"Nnq7C weEfm"}).find_all(href = True)
+                links = soup.find("div", {"class": "Nnq7C weEfm"}).find_all(href=True)
 
             i = 1
             for link in links[0:3]:
                 try:
                     url = link["href"]
-                    self.driver.get(os.path.join(self.insta_url,url[1:]))
-                    self.df.loc[handle,"post_{}_content".format(i)] = WebDriverWait(self.driver,5).until(ec.presence_of_element_located((By.XPATH,"//div[@class='C7I1f X7jCj']"))).text
-                    i += 1 
-                    time.sleep(np.random.exponential(1,1))
+                    self.driver.get(os.path.join(self.insta_url, url[1:]))
+                    self.df.loc[handle, "post_{}_content".format(i)] = (
+                        WebDriverWait(self.driver, 5)
+                        .until(
+                            ec.presence_of_element_located(
+                                (By.XPATH, "//div[@class='C7I1f X7jCj']")
+                            )
+                        )
+                        .text
+                    )
+                    i += 1
+                    time.sleep(np.random.exponential(1, 1))
                 except:
                     i += 1
-                    self.df.loc[handle,"post_{}_content".format(i)] = os.path.join(self.insta_url,url[1:])
+                    self.df.loc[handle, "post_{}_content".format(i)] = os.path.join(
+                        self.insta_url, url[1:]
+                    )
                     continue
 
     def process_data_categorically(self,):
